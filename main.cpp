72f119aa6e0f0cf59a84222dbec5cb664cf0796a
Minh <95263712+minhng01@users.noreply.github.com>
2021-12-07 01:25:35 +0000

    Completed part 2.

--- main.cpp
+++ main.cpp
@@ -107,9 +107,51 @@ struct CarWash
     you should be able to deduce the return type of those functions based on their usage in Person::run()
     You'll need to insert the Person struct from the video in the space below.
  */
+struct Person
+{
+    int age;
+    int height;
+    float hairLength;
+    float GPA;
+    unsigned int SATScore;
+    int distanceTraveled;
 
+    struct Foot
+    {
+        int size = 3;
 
+        int stepSize()
+        {
+            return size;
+        }
 
+        void stepForward()
+        {
+            // do something
+        }
+    };
+
+    Foot leftFoot;
+    Foot rightFoot;
+
+    void run(int howFast, bool startWithLeftFoot);
+};
+
+void Person::run(int howFast, bool startWithLeftFoot)
+{
+    if (startWithLeftFoot == true)
+    {
+        leftFoot.stepForward();
+        rightFoot.stepForward();
+    }
+    else
+    {
+        rightFoot.stepForward();
+        leftFoot.stepForward();
+    }
+
+    distanceTraveled += (leftFoot.stepSize() + rightFoot.stepSize()) * howFast; // howFast indicates ratio of stepSize in this case
+}
 
 
  /*
@@ -157,6 +199,94 @@ struct OvenStove
     FoodItem mainIngredient;
 };
 
+void OvenStove::FoodItem::clean(std::string cleaningType)
+{
+    if (expirationDayRemain <= 0)
+    {
+        std::cout << "Expired food!" << std::endl;
+        return;
+    }
+
+    std::cout << "Cleaning with " + cleaningType << std::endl;
+}
+
+void OvenStove::FoodItem::season(std::string mainCondiment)
+{
+    if (expirationDayRemain <= 0)
+    {
+        std::cout << "Expired food!" << std::endl;
+        return;
+    }
+
+    if (isSeasoned == false)
+    {
+        std::cout << "Season with " + mainCondiment << std::endl;
+        isSeasoned = true;
+    }
+
+}
+
+void OvenStove::FoodItem::cut(int numberOfPortion, std::string tool)
+{
+    if (expirationDayRemain <= 0)
+    {
+        std::cout << "Expired food!" << std::endl;
+        return;
+    }
+
+    if (numberOfPortion > 1)
+    {
+        std::cout << "Cut the food into " 
+                + std::to_string(numberOfPortion) 
+                + " with " 
+                + tool 
+                << std::endl;
+    }
+    else
+    {
+        std::cout << "Do nothing" << std::endl;
+    }
+}
+
+void OvenStove::broilFood(OvenStove::FoodItem food, int temperature, int durationInMinute)
+{
+    std::cout << "Broiling " + food.name 
+                    + " at " + std::to_string(temperature) + "F"
+                    + " in " + std::to_string(durationInMinute) + "minutes";
+}
+
+void bakeFood(OvenStove::FoodItem food, int temperature, int durationInMinute, int rackId, bool preHeat)
+{
+    if (rackId < 0)
+    {
+        std::cout << "ERROR!" << std::endl;
+        return;
+    }
+
+    if (preHeat == false)
+    {
+        std::cout << "Please pre heat oven before start!" << std::endl;
+        return;
+    }
+
+    std::cout << "Baking " + food.name 
+                    + " at " + std::to_string(temperature) + "F"
+                    + " in " + std::to_string(durationInMinute) + "minutes";
+}
+
+void lightUp(bool lightStatus)
+{
+    if (lightStatus == false)
+    {
+        std::cout << "Light ON" << std::endl;
+        lightStatus = true;
+    }
+    else
+    {
+        std::cout << "Do NOTHING" << std::endl;
+    }
+}
+
 struct Laptop
 {
     std::string modelName = "Thinkpad";
@@ -170,6 +300,44 @@ struct Laptop
     void playAudio(int audioFileId = 0, int volume = 50, std::string playMode = "Headphones");
 };
 
+void Laptop::connectToWifi(bool connectionStatus)
+{
+    if (connectionStatus == false)
+    {
+        connectionStatus = true;
+    }
+    std::cout << "You are connected to the Internet" << std::endl;
+}
+
+std::string Laptop::checkAvailableOSUpdate(int currentOSId)
+{
+    if (currentOSId < 10) // assume 10 is the latest OS id
+    {
+        return "Update version 10.";
+    }
+    else
+    {
+        return "Up to date!";
+    }
+}
+
+void Laptop::playAudio(int audioFileId, int volume, std::string playMode)
+{
+    if (audioFileId < 0 || audioFileId > 99) // assume there is 100 audio files 
+    {
+        return;
+    }
+
+    if (volume > 70)
+    {
+        std::cout << "Warning: audio too loud!" << std::endl;
+    }
+
+    std::cout << "Playing audio " + std::to_string(audioFileId) 
+                + "at channel " + playMode 
+                << std::endl; 
+}
+
 struct FruitTree
 {
     std::string name = "Apple Tree";
@@ -182,6 +350,37 @@ struct FruitTree
     void absorbCarbonDioxide(int currentDayTimeInHour = 21);
     int produceFruit(std::string currentSeason = "December");
 };
+
+void FruitTree::produceOxygen(int currentDayTimeInHour)
+{
+    if (currentDayTimeInHour > 5 && currentDayTimeInHour < 18) // from 6:00 to 17:59
+    {
+        std::cout << "Day time!" << std::endl;
+        std::cout << name << "is producing oxygen.";
+    }
+}
+
+void FruitTree::absorbCarbonDioxide(int currentDayTimeInHour)
+{
+    if (currentDayTimeInHour > 5 && currentDayTimeInHour < 18) // from 6:00 to 17:59
+    {
+        std::cout << "Day time!" << std::endl;
+        std::cout << name << "is absorbing carbon dioxide.";
+    }
+}
+
+int FruitTree::produceFruit(std::string currentSeason)
+{
+    if (currentSeason == harvestSeason)
+    {
+        return 1000;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 
 struct Camera
 {
@@ -204,12 +403,81 @@ struct Camera
         void getFocus(double distanceToObject);
     };
 
-    void shootPhoto(Lens currentLens, char shootingMode = 'A');
+    void shootPhoto(Lens currentLens, char shootingMode = 'A', bool lowLight = false);
     void recordVideo(Lens currentLens, int durationInSecond);
     void playFlash(bool lowLightIntensity = true);
 
     Lens includedLens;
 };
+
+void Camera::Lens::cover()
+{
+    // do something to cover
+}
+
+void Camera::Lens::zoom(float zoomMode)
+{
+    if (zoomMode < 1.0f)
+    {
+        std::cout << "Zoom OUT" << std::endl;
+    }
+    else if (zoomMode > 1.0f)
+    {
+        std::cout << "Zoom IN" << std::endl;
+    }
+    else {
+        std::cout << "Original" << std::endl;
+    }
+}
+
+void Camera::Lens::getFocus(double distanceToObject)
+{
+    if (distanceToObject < 1)
+    {
+        std::cout << "Too close. Please move further." << std:: endl;
+    }
+    else if (distanceToObject > 100)
+    {
+        std::cout << "Too far. Please move closer." << std:: endl;
+    }
+    else
+    {
+        // get focus on object
+        std::cout << "Ready." << std::endl;
+    }
+}
+
+void Camera::shootPhoto(Camera::Lens currentLens, char shootingMode, bool lowLight)
+{
+    currentLens.getFocus(50); // assume default value 50 for photo
+    if (shootingMode == 'A')
+    {
+        std::cout << "Auto mode";
+        playFlash(lowLight); // auto mode play flash if low light
+    }
+    else if (shootingMode == 'M')
+    {
+        std::cout << "Please choose shutter speed" << std::endl;
+        std::cout << "Flash option";
+    }
+}
+
+void Camera::recordVideo(Camera::Lens currentLens, int durationInSecond)
+{
+    currentLens.getFocus(70); // assume default value 70 for video
+    std::cout << "Recording..." << std::endl;
+    // record video
+    std::cout << "Duration: " + std::to_string(durationInSecond) + " Seconds"; 
+}
+
+void Camera::playFlash(bool lowLightIntensity)
+{
+    if (lowLightIntensity == true)
+    {
+        // turn on Flash
+    }
+}
+
 
 struct Cooktop
 {
@@ -224,6 +492,31 @@ struct Cooktop
     void boilWater(int burnerId, int durationInSecond);
 };
 
+void Cooktop::burnFood()
+{
+    std::cout << "Warning: Hot surface.";
+}
+
+double Cooktop::consumeElectric(bool isConnectedToSource)
+{
+    if (isConnectedToSource)
+    {
+        return 120; //assume value
+    }
+    return 0;
+}
+
+void Cooktop::boilWater(int burnerId, int durationInSecond)
+{
+    if (burnerId < 0 || burnerId >= numberOfBurner) 
+    {
+        return;
+    }
+    std::cout << "Boiling water at burner no." + std::to_string(burnerId)
+                + " in " + std::to_string(durationInSecond) + " seconds.";
+}
+
+
 struct Oven
 {
     int numberOfRack = 2;
@@ -236,6 +529,36 @@ struct Oven
     void bake(int rackId, int temperatureInFarenheit, int durationInMinute);
     void selfClean();
 };
+
+void Oven::broil(int temperatureInFarenheit, int durationInMinute)
+{
+    std::cout << "Broiling in " + std::to_string(durationInMinute) 
+                    + " at " + std::to_string(temperatureInFarenheit) + "F";
+}
+
+void Oven::bake(int rackId, int temperatureInFarenheit, int durationInMinute)
+{
+    if (rackId < 0 || rackId >= numberOfRack)
+    {
+        return;
+    } 
+    if (durationInMinute > 300)
+    {
+        std::cout << "Warning: Too long. Might need to shorten time.";
+    }
+    if (temperatureInFarenheit > 450)
+    {
+        std::cout << "Max temperature.";
+        temperatureInFarenheit = 450;
+    }
+    std::cout << "Baking at " + std::to_string(temperatureInFarenheit)+ "F ...";
+}
+
+void Oven::selfClean()
+{
+    std::cout << "Self-cleaning...";
+}
+
 
 struct Control
 {
@@ -250,6 +573,26 @@ struct Control
     void turnOnOven(bool isOvenOn = false);
 };
 
+int Control::adjustOvenTemperature(int currentTemperature, int numberOfUpButtonClick, int numberOfDownButtonClick)
+{
+    return (currentTemperature + 5 * (numberOfUpButtonClick - numberOfDownButtonClick)); // up and down in the unit of 5 degree
+}
+
+int Control::changeCookingTimer(int currentTimer, int numberOfUpButtonClick, int numberOfDownButtonClick)
+{
+    return (currentTimer + numberOfUpButtonClick - numberOfDownButtonClick);
+}
+
+void Control::turnOnOven(bool isOvenOn)
+{
+    if (isOvenOn == false)
+    {
+        std::cout << "Turning on...";
+        ovenPowerOnLightColor = "Orange";
+    }
+}
+
+
 struct RangeHood
 {
     float chimneyHeight = 30.5f;
@@ -262,6 +605,33 @@ struct RangeHood
     void turnLightOn();
     int controlFanStrength(int currentStrengthIndicator, int numberOfCounterClockwiseTurn, int numberOfClockWiseTurn);
 };
+
+void RangeHood::suckSmoke(int fanStrengthLevel)
+{
+    if (fanStrengthLevel < (numberOfFanStrengthLevel - 1)) // from 0 to number of level
+    {
+        std::cout << "Fan is ON.";
+    }
+}
+
+void RangeHood::turnLightOn()
+{
+    std::cout << "Light is ON.";
+}
+
+int RangeHood::controlFanStrength(int currentStrengthIndicator, int numberOfCounterClockwiseTurn, int numberOfClockWiseTurn)
+{
+    if (currentStrengthIndicator == 0)
+    {
+        return (currentStrengthIndicator + numberOfClockWiseTurn);
+    }
+    if (currentStrengthIndicator == numberOfFanStrengthLevel - 1)
+    {
+        return (currentStrengthIndicator - numberOfCounterClockwiseTurn);
+    }
+    return (currentStrengthIndicator - numberOfCounterClockwiseTurn + numberOfClockWiseTurn);
+}
+
 
 struct UnderStoveStorage
 {
@@ -276,6 +646,27 @@ struct UnderStoveStorage
     void containBakeware(std::string bakewareName = "Baking pan", bool isFull = false);
 };
 
+void UnderStoveStorage::slideOut()
+{
+    std::cout << "Sliding out.";
+}
+
+void UnderStoveStorage::slideIn()
+{
+    std::cout << "Sliding in.";
+}
+
+void UnderStoveStorage::containBakeware(std::string bakewareName, bool isFull)
+{
+    if (isFull == true)
+    {
+        return;
+    }
+    std::cout << "Put " + bakewareName + "in.";
+}
+
+
+
 struct KitchenRange
 {
     Cooktop cookTop;
@@ -285,9 +676,29 @@ struct KitchenRange
     UnderStoveStorage drawer;
 
     void bakeFood(int foodId, int rackId, int temperatureInFarenheit, int durationInMinute);
-    void lightUp(int lightIntensity = 1);
+    void lightUp();
     void ventSmoke();
 };
+
+void KitchenRange::bakeFood(int foodId, int rackId, int temperatureInFarenheit, int durationInMinute)
+{
+    if (foodId < 0 || foodId > 9) // assume only have 10 food items
+    {
+        return;
+    }
+    std::cout << "Start baking food item " + std::to_string(foodId) + " now."<< std::endl;
+    oven.bake(rackId, temperatureInFarenheit, durationInMinute);
+}
+
+void KitchenRange::lightUp()
+{
+    rangeHood.turnLightOn();
+}
+
+void KitchenRange::ventSmoke()
+{
+    rangeHood.suckSmoke(1); // default of fan strength level is 1
+}
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
